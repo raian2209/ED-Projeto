3b6a6de3b267716d36eb327f6bea7361c108b3ad
Edson Alencar <edson.cesar.alencar@gmail.com>
2025-11-24 20:09:14 -0300

    Fix: corrige back para aceitar dado no formData padrão de envio do front end e liberar cors no controller

--- huffman-compressor/src/main/java/com/exemplo/huffman/controller/HuffmanController.java
+++ huffman-compressor/src/main/java/com/exemplo/huffman/controller/HuffmanController.java
@@ -3,27 +3,46 @@ package com.exemplo.huffman.controller;
 
 import com.exemplo.huffman.service.HuffmanService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
 @RestController
 @RequestMapping("/huffman")
+@CrossOrigin("*")
 public class HuffmanController {
 
     @Autowired
     private HuffmanService huffmanService;
 
-    @PostMapping("/compress")
-    public byte[] compress(@RequestBody byte[] data) throws IOException {
-        return huffmanService.compress(data);
+    @PostMapping(value = "/compress", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<byte[]> compress(@RequestParam("file") MultipartFile file) throws IOException {
+        byte[] originalData = file.getBytes();
+
+        byte[] compressedData = huffmanService.compress(originalData);
+
+        return ResponseEntity.ok()
+            .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + file.getOriginalFilename() + ".huff")
+            .contentType(MediaType.APPLICATION_OCTET_STREAM)
+            .body(compressedData);
     }
 
-    @PostMapping("/decompress")
-    public byte[] decompress(@RequestBody byte[] data) throws IOException {
-        return huffmanService.decompress(data);
+    @PostMapping(value = "/decompress", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<byte[]> decompress(@RequestParam("file") MultipartFile file) throws IOException {
+        byte[] compressedData = file.getBytes();
+
+        byte[] decompressedData = huffmanService.decompress(compressedData);
+
+        // Tenta remover a extensão .huff para o nome original, se possível
+        String originalName = file.getOriginalFilename().replace(".huff", "");
+
+        return ResponseEntity.ok()
+            .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + originalName)
+            .contentType(MediaType.APPLICATION_OCTET_STREAM)
+            .body(decompressedData);
     }
 }
